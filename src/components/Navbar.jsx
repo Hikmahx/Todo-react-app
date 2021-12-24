630ea818d0c68ce8e7920da47c207ceec2f7f1ad
Hikmahx <hikmayousuph@gmail.com>
2021-12-24 23:33:54 +0100

    navbar component completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import bgMobileLight from '../assets/bg-mobile-light.jpg'
+import bgMobileDark from '../assets/bg-mobile-dark.jpg'
+import bgDesktopLight from '../assets/bg-desktop-light.jpg'
+import bgDesktopDark from '../assets/bg-desktop-dark.jpg'
+import moon from '../assets/icon-moon.svg'
+
+
+const Navbar = () => {
+  return (
+    <header className="w-full relative flex justify-center">
+      <div className="header-background">
+        <img src={bgMobileLight} alt="header-img" className="absolute inset-0 object-cover w-full lg:hidden" />
+        <img src={bgDesktopLight} alt="header-img" className="absolute inset-0 object-cover w-full hidden lg:block" />
+        <div className="screen  bg-gradient-to-r from-gradient-blue to-gradient-purple absolute inset-0 opacity-50"></div>
+      </div>
+      <div className="w-full max-w-xl mx-auto absolute flex items-center justify-between py-11 lg:py-16 lg:mt-4 px-6 lg:px-4">
+        <div className="logo uppercase text-white text-3xl lg:text-4xl font-bold tracking-wider lg:tracking-widest">todo</div>
+        <img src={moon} alt="moon-icon" className="p-1 w-7 lg:w-8" />
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
