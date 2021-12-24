1c28e43ad29fa3d3290aa5b85a047c0c40f6ed17
Hikmahx <hikmayousuph@gmail.com>
2021-12-25 00:18:18 +0100

    darkMode enabled in header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import bgMobileDark from '../assets/bg-mobile-dark.jpg'
 import bgDesktopLight from '../assets/bg-desktop-light.jpg'
 import bgDesktopDark from '../assets/bg-desktop-dark.jpg'
 import moon from '../assets/icon-moon.svg'
+import sun from '../assets/icon-sun.svg'
 
 
 const Navbar = ({modeToggle, darkMode}) => {
@@ -11,13 +12,13 @@ const Navbar = ({modeToggle, darkMode}) => {
   return (
     <header className="w-full relative flex justify-center">
       <div className="header-background">
-        <img src={bgMobileLight} alt="header-img" className="absolute inset-0 object-cover w-full lg:hidden" />
-        <img src={bgDesktopLight} alt="header-img" className="absolute inset-0 object-cover w-full hidden lg:block" />
-        <div className="screen  bg-gradient-to-r from-gradient-blue to-gradient-purple absolute inset-0 opacity-50"></div>
+        <img src={darkMode? bgMobileLight : bgMobileDark} alt="header-img" className="absolute inset-0 object-cover w-full lg:hidden" />
+        <img src={darkMode? bgDesktopLight: bgDesktopDark} alt="header-img" className="absolute inset-0 object-cover w-full hidden lg:block" />
+        {/* <div className={ "screen" + darkMode? "bg-gradient-to-r from-gradient-blue to-gradient-purple" : "bg-gradient-to-r from-gradient-purple to-gradient-blue" + " absolute inset-0 opacity-50"}></div> */}
       </div>
       <div className="w-full max-w-xl mx-auto absolute flex items-center justify-between py-11 lg:py-16 lg:mt-4 px-6 lg:px-4">
         <div className="logo uppercase text-white text-3xl lg:text-4xl font-bold tracking-wider lg:tracking-widest">todo</div>
-        <img onClick={modeToggle} src={moon} alt="moon-icon" className="p-1 w-7 lg:w-8" />
+        <img onClick={modeToggle} src={darkMode? moon: sun} alt="moon-icon" className="p-1 w-7 lg:w-8" />
       </div>
     </header>
   )
