299bea725edb23bd879f1186406d76b986769b9c
Hikmahx <hikmayousuph@gmail.com>
2021-12-24 19:06:20 +0100

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
-  return <div className='App'>
-  </div>;
+  return( 
+  <div className='App bg-gray dark:bg-very-dark-blue font-josefinSans'>
+    <Navbar/>
+  </div>
+  )
 }
 
 export default App;
