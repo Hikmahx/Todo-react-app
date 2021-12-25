a24476c0f81c10e0be996d97b700d12b14135e30
Hikmahx <hikmayousuph@gmail.com>
2021-12-25 23:58:06 +0100

    style updated for mobile and tablet view

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,15 +18,15 @@ const Form = () => {
   const [checked, setChecked] = useState(true)
 
   return (
-    <div style={{maxWidth:"34rem", top:"0.1rem"}} className="-mt-36 relative mx-6 md:mx-auto h-16 bg-white dark:bg-very-dark-desaturated-blue transition-colors rounded flex items-center">
-      <form className="w-full mx-6 flex">
+    <div style={{maxWidth:"34rem", top:"0.1rem"}} className="form-wrapper -mt-24 md:-mt-28 lg:-mt-36 relative mx-6 md:mx-auto h-12 lg:h-16 bg-white dark:bg-very-dark-desaturated-blue transition-colors rounded flex items-center">
+      <form className="w-full mx-5 lg:mx-6 flex items-center">
         <span  className="checkbox-wrapper relative cursor-pointer">
-          <div className=" relative bg-very-light-grayish-blue dark:bg-very-dark-grayish-blue-dark hover:bg-gradient-to-r hover:from-gradient-blue hover:to-gradient-purple transition-colors rounded-full w-6 h-6 flex items-center justify-center">
-            <img src={check} alt="check-icon" className="w-3 h-3" />
-            <div onClick={checkBox} className=" checkbox absolute inset-0 bg-white dark:bg-very-dark-desaturated-blue transition-colors w-5 h-5 rounded-full m-auto"></div>
+          <div className=" relative bg-very-light-grayish-blue dark:bg-very-dark-grayish-blue-dark hover:bg-gradient-to-r hover:from-gradient-blue hover:to-gradient-purple transition-colors rounded-full w-5 h-5 lg:w-6 lg:h-6 flex items-center justify-center">
+            <img src={check} alt="check-icon" className="w-2 h-2 lg:w-3 lg:h-3" />
+            <div onClick={checkBox} className=" checkbox absolute inset-0 bg-white dark:bg-very-dark-desaturated-blue transition-colors w-4 h-4 lg:w-5 lg:h-5 rounded-full m-auto"></div>
           </div>
         </span>
-        <input type="text" className="bg-transparent w-full focus:outline-none px-6 text-lg text-darkest-grayish-blue dark:text-gray" placeholder="Create a new todo..." />
+        <input type="text" className="bg-transparent w-full focus:outline-none px-3 pt-1 lg:px-6 text-xs sm:text-base lg:text-lg text-darkest-grayish-blue dark:text-gray" placeholder="Create a new todo..." />
       </form>
     </div>
   )
