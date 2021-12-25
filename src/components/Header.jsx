4e077b250f458506d5cf1afaee29c0d4e02bc259
Hikmahx <hikmayousuph@gmail.com>
2021-12-26 00:26:35 +0100

    style updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,12 +11,12 @@ const Header = ({modeToggle, darkMode}) => {
 
   return (
     <header className="w-full relative flex justify-center">
-      <div className="header-background">
+      <div className="header-background w-full">
         <img src={darkMode? bgMobileLight : bgMobileDark} alt="header-img" className="relative inset-0 object-cover w-full lg:hidden" />
         <img src={darkMode? bgDesktopLight: bgDesktopDark} alt="header-img" className="relative inset-0 object-cover w-full hidden lg:block" />
         {/* <div className={ "screen" + darkMode? "bg-gradient-to-r from-gradient-blue to-gradient-purple" : "bg-gradient-to-r from-gradient-purple to-gradient-blue" + " absolute inset-0 opacity-50"}></div> */}
       </div>
-      <div className="w-full max-w-xl mx-auto absolute flex items-center justify-between py-11 lg:py-16 lg:mt-4 px-6 lg:px-4">
+      <div className="w-full max-w-xl mx-auto absolute flex items-center justify-between py-11 xl:py-16 lg:mt-4 px-6 lg:px-4">
         <div className="logo uppercase text-white text-3xl lg:text-4xl font-bold tracking-wider lg:tracking-widest">todo</div>
         <img onClick={modeToggle} src={darkMode? moon: sun} alt="moon-icon" className="p-1 w-7 lg:w-8 cursor-pointer" />
       </div>
